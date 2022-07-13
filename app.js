d819cb1da2ec9d1d0146b25fab38a3bd5efa467c
Maxwell Rutherford <javert.hugo@yandex.ru>
2022-07-13 19:57:11 +0300

    add env and userSchema

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+require('dotenv').config();
 
 app.use(express.json({ extended: true }))
 
 
 app.use('/auth', require('./routes/auth.routes'))
 
+const PORT = process.env.PORT || 3000
+
 const start = () => {
     try {
-        app.listen(3000, () => {
-            console.log('app has been started on port 3000')
+
+        mongoose.connect(process.env.NODE_ENV === "production" ? 
+            process.env.DB_PROD_CONN_URI :
+            process.env.DB_DEV_CONN_URI
+        , {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
+
+        app.listen(PORT, () => {
+            console.log(`app has been started on port ${PORT}`)
         })
     } catch (error) {
         console.error(error)
